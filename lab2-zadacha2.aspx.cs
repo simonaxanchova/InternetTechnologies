c4e4733686cd2e1e042579de494ded43da736b29
Simona Anchova <62893553+simonaxanchova@users.noreply.github.com>
2021-03-31 13:53:38 +0200

    Update lab2-zadacha2.aspx.cs

--- lab2-zadacha2.aspx.cs
+++ lab2-zadacha2.aspx.cs
@@ -24,26 +24,16 @@ namespace Lab2
             MultiView1.ActiveViewIndex -= 1;
         }
 
-        protected void btnPodnesi_Click(object sender, EventArgs e)
-        {
-            int workingExperience;
-            if (int.TryParse(txtGodini.Text, out workingExperience) && workingExperience >= 5)
-            {
-                lblKorisnik.Text = txtID.Text;
-                lblKorisnik.Text = lblMail.Text;
-                lblUspesnoKreiranje.Visible = true;
-            }
-            else
-            {
-                lblUspesnoKreiranje.Visible = false;
-                lblGreska.Text = "Немате доволно искуство";
-            }
-            btnNext_Click(sender, e);
-        }
 
-        protected void btnPocetok_Click(object sender, EventArgs e)
+        protected void btnStart_Click(object sender, EventArgs e)
         {
             MultiView1.ActiveViewIndex = 0;
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            
+
         }
     }
 }
