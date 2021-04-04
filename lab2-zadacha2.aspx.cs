1628d195a9f3450d1569f34a9be06680a2954c4e
Simona Anchova <62893553+simonaxanchova@users.noreply.github.com>
2021-04-04 21:40:46 +0200

    Update lab2-zadacha2.aspx.cs

--- lab2-zadacha2.aspx.cs
+++ lab2-zadacha2.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Lab2
+namespace kolokviumskiVezhbi
 {
-    public partial class zadacha2 : System.Web.UI.Page
+    public partial class lab2zadacha2 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -24,16 +24,26 @@ namespace Lab2
             MultiView1.ActiveViewIndex -= 1;
         }
 
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            int workingExperience;
+            if (int.TryParse(years.Text, out workingExperience) && workingExperience >= 5)
+            {
+                user.Text = finkiID.Text;
+                userMail.Text = lblMail.Text;
+                successRegistration.Visible = true;
+            }
+            else
+            {
+                successRegistration.Visible = false;
+                error.Text = "Немате доволно работно искуство!";
+            }
+            btnNext_Click(sender, e);
+        }
 
         protected void btnStart_Click(object sender, EventArgs e)
         {
             MultiView1.ActiveViewIndex = 0;
-        }
-
-        protected void Submit_Click(object sender, EventArgs e)
-        {
-            
-
         }
     }
 }
