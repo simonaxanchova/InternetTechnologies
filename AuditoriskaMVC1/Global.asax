3991449535d12721fa3efdd1e3804bcc7de3fa22
Simona Anchova <anchovasimona@yahoo.com>
2021-05-10 21:27:15 +0200

    Adding MVC app

---
+++ AuditoriskaMVC1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AuditoriskaMVC1.MvcApplication" Language="C#" %>
---
+++ AuditoriskaMVC1/Migrations/202105101834337_initial.cs
@@ -0,0 +1,44 @@
+﻿namespace AuditoriskaMVC1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clients",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        NameClient = c.String(nullable: false),
+                        Address = c.String(nullable: false),
+                        Age = c.Int(nullable: false),
+                        MovieCard = c.String(),
+                        Telephone = c.String(),
+                        isSubscribed = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Rating = c.Single(nullable: false),
+                        DownloadURL = c.String(),
+                        ImageUrl = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+            DropTable("dbo.Clients");
+        }
+    }
+}
