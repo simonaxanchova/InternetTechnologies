6fc867dc258f1a8cfa3d4c5869c58607e244137c
Simona Anchova <62893553+simonaxanchova@users.noreply.github.com>
2021-05-10 20:49:07 +0200

    Add files via upload

---
+++ Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AuditoriskaMVC1.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public float Rating { get; set; }
+
+        [Display(Name = "Download URL")]
+        public string DownloadURL { get; set; }
+
+        [Required]
+        [Display(Name = "The Image URL")]
+        public string ImageUrl { get; set; }
+    }
+}
