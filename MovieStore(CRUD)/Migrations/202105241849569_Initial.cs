958420d5ed7c259869f54aca16a1c9c35156bbba
Simona Anchova <anchovasimona@yahoo.com>
2021-05-24 21:59:14 +0200

    Adding MovieStore CRUD

---
+++ MovieStore(CRUD)/Migrations/202105241849569_Initial.cs
@@ -0,0 +1,43 @@
+namespace MovieStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clients",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Address = c.String(nullable: false),
+                        Telephone = c.String(),
+                        MovieCard = c.String(),
+                        Age = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        DownloadURL = c.String(),
+                        Rating = c.Single(nullable: false),
+                        ImageURL = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+            DropTable("dbo.Clients");
+        }
+    }
+}
---
+++ MovieStore(CRUD)/Models/ClientMovie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieStore.Models
+{
+    public class ClientMovie
+    {
+        public int MovieId { get; set; }
+        public int ClientId { get; set; }
+        public Movie movie { get; set; }
+        public List<Client> Clients { get; set; }
+    }
+}
